64f735634f794bdc915ded65ead410d72fd0bb56
Renato Freitas <renatjf@gmail.com>
2018-05-23 20:39:01 -0300

    teste de props

--- components-vue/src/App.vue
+++ components-vue/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">    
     <app-component01></app-component01>
+    <app-component02 testeProps="Texto da props"></app-component02>    
   </div>
 </template>
 
 <script>
 import Component01 from './components/Component01'
+import Component02 from './components/Component02'
 
 export default {
   name: 'App',
   components: {
-    "app-component01": Component01    
+    "app-component01": Component01,
+    "app-component02": Component02
   }
 }
 </script>
---
+++ components-vue/src/components/Component02.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="teste">
+    <h1>{{ msg }} - {{ testeProps }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Component02',
+  props: ['testeProps'],
+  data () {
+    return {
+      msg: 'Component 02 Props',
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .teste {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 20px;
+  }
+</style>
