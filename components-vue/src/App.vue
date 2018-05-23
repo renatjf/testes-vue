89eb69ea016121924f567870c90c32e42fa94904
Renato Freitas <renatjf@gmail.com>
2018-05-23 20:22:19 -0300

    news

---
+++ components-vue/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">    
+    <app-component01></app-component01>
+  </div>
+</template>
+
+<script>
+import Component01 from './components/Component01'
+
+export default {
+  name: 'App',
+  components: {
+    "app-component01": Component01    
+  }
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;    
+}
+</style>
---
+++ components-vue/src/components/Component01.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="teste">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Component01',
+  data () {
+    return {
+      msg: 'Component 01 Normal'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .teste {
+    color: red;
+  }
+</style>
