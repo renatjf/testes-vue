581c2f7d851d4dc1f7ba6e2f8e70b7c9f27a6501
Renato Freitas <renatjf@gmail.com>
2018-05-28 17:43:21 -0300

    ajustes

---
+++ netflix_array_dimanico/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div class="contain">
+      <app-terror></app-terror>      
+    </div>
+  </div>
+</template>
+
+<script>
+//import component
+import Terror from "./components/Terror";
+
+export default {
+  name: "app",
+
+  //declara components
+  components: {
+    "app-terror": Terror
+  },
+  data() {
+    return {
+      msg: "Teste Netflix"
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+</style>
