bbd65fefbddf8eed6fa696985def5545a3054e08
Renato Freitas <renatjf@gmail.com>
2018-06-11 21:14:33 -0300

    styled

---
+++ styled-component/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <app-testeStyled></app-testeStyled>
+  </div>
+</template>
+
+<script>
+import TesteStyled from './components/TesteStyled'
+export default {
+  name: 'app',
+  components: {
+    "app-testeStyled": TesteStyled
+  },
+  data () {
+    return {      
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {  
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;    
+  }
+</style>
---
+++ styled-component/src/components/TesteStyled.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <StyledTitle>
+      <h1>Teste</h1>
+    </StyledTitle>
+  </div>
+</template>
+
+<script>
+import styled from "vue-styled-components";
+
+const StyledTitle = styled.h1`
+  font-size: 50px;
+  text-align: center;
+  color: red;
+`;
+
+export default {
+  name: "TesteStyled",
+  components: {
+    StyledTitle
+  },
+  
+  data() {
+    return {
+      msg: "Teste Styled components"
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
