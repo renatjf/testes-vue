36102249cd226bd91b129ca663daaa6373dabee7
Renato <renatjf@gmail.com>
2019-08-12 16:38:14 -0300

    ajuste

--- vue-styled-component-avancado/src/styled/Teste.js
+++ vue-styled-component-avancado/src/styled/Teste.js
@@ -2,13 +2,12 @@ import styled from 'vue-styled-components'
 import * as vars from './variables'
 
 const Teste = styled.div`
-   color: ${vars.COLOR_YELLOW};
-   font-family: Arial, Helvetica, sans-serif;
+  color: ${vars.COLOR_YELLOW};
+  font-family: Arial, Helvetica, sans-serif;
 
-   .teste {
-       color: red
-   }
-
+  .teste {
+    color: red
+  }
 ` ;
 
 export default Teste
